07f0725464e7d9dbf6bfea7295026337f4b9da86
James (Jamalam) <james@jamalam.tech>
2023-10-29 21:58:09 +0000

    fix: remove platform specific stuff

--- fabric/src/main/java/io/github/jamalam360/quickerconnectbutton/fabric/FabricExpectPlatformImpl.java
+++
@@ -1,7 +0,0 @@
-package io.github.jamalam360.quickerconnectbutton.fabric;
-
-import io.github.jamalam360.quickerconnectbutton.QuickerConnectButtonPlatform;
-
-public class FabricExpectPlatformImpl {
-	
-}
--- forge/src/main/java/io/github/jamalam360/quickerconnectbutton/forge/ForgeExpectPlatformImpl.java
+++
@@ -1,7 +0,0 @@
-package io.github.jamalam360.quickerconnectbutton.forge;
-
-import io.github.jamalam360.quickerconnectbutton.QuickerConnectButtonPlatform;
-
-public class ForgeExpectPlatformImpl {
-	
-}
